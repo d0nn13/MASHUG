c8c28720b69a976f866a1f4a2a534aeef2476d67
Samir Ahamada <ahamad_s@etna-alternance.net>
2014-04-21 12:44:15 +0200

    Some skeleton files

---
+++ src/core/sound/bgm.h
@@ -0,0 +1,15 @@
+/*
+** bgm.h for SpaceInvaders in /Users/ahamad_s/dev/ETNA/Projets/TCM-DEVC/SpaceInvaders/src
+** 
+** Made by AHAMADA Samir
+** Login   <ahamad_s@etna-alternance.net>
+** 
+** Started on  Sun Apr 20 22:01:38 2014 AHAMADA Samir
+** Last update Sun Apr 20 22:02:17 2014 AHAMADA Samir
+*/
+
+#ifndef BGM_H
+#define BGM_H
+
+#endif /* !BGM_H */
+
