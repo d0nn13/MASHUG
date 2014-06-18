1950861b6b7290bfe563128712a823869727b0b9
Samir Ahamada <ahamad_s@etna-alternance.net>
2014-06-18 18:13:15 +0200

    Revert "Moved catstring array initialisation outside of function"
    This reverts commit 2ab3ba054b8bfe170dca69a2ead029baeb38dd00.
    
    Conflicts:
    	src/core/log.c

--- src/core/log.c
+++ src/core/log.c
@@ -17,24 +17,7 @@
  *	String containing timestamp
  */
 static char	timestr[9];
-
-/**
- * Beware of the order! See macro and enum in log.h
- */
-static char	*catstrings[NB_LCAT] = 
-{
-  "",
-  "Space Invaders",
-  "Error",
-  "System",
-  "Audio",
-  "Video",
-  "Render",
-  "Input",
-  "Graphics",
-  "Sprites",
-  "Fonts"
-};
+static char	*catstrings[NB_LCAT];
 
 /**
  *	Writes a timestamp into 'timestr'
@@ -60,6 +43,16 @@ void	log_init()
   SDL_LogSetPriority(SPR_LCAT, SDL_LOG_PRIORITY_INFO);
   SDL_LogSetPriority(INP_LCAT, SDL_LOG_PRIORITY_INFO);
 #endif
+  catstrings[APP_LCAT] = "Space Invaders";
+  catstrings[ERR_LCAT] = "Error";
+  catstrings[SYS_LCAT] = "System";
+  catstrings[AUD_LCAT] = "Audio";
+  catstrings[VID_LCAT] = "Video";
+  catstrings[RDR_LCAT] = "Render";
+  catstrings[INP_LCAT] = "Input";
+  catstrings[GFX_LCAT] = "Graphics";
+  catstrings[SPR_LCAT] = "Sprites";
+  catstrings[FNT_LCAT] = "Fonts";
 }
 
 static void	timestamp()
