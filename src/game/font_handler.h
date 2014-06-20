de691add928184ebc9f88b8f44235e1bbead2c62
Ennebati <y.ennebati@gmail.com>
2014-06-20 20:20:03 +0200

    [Doc/folderGame] Added annotations for font_handler.h

--- src/game/font_handler.h
+++ src/game/font_handler.h
@@ -5,7 +5,7 @@
 ** Login   <ahamad_s@etna-alternance.net>
 ** 
 ** Started on  Mon Apr 28 15:34:14 2014 AHAMADA Samir
-** Last update Thu Jun 19 22:10:54 2014 AHAMADA Samir
+** Last update Sat Jun 21 02:19:45 2014 ENNEBATI Yassine
 */
 
 /**
@@ -21,23 +21,19 @@
 /**
  *	Allocates a new surface with 'text' printed with given
  *	font 'f' and color 'c'.
- *  @param text a constant character pointer.
- *
- *  @param f TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
- *
- *  @param c a SDL_Color structure pointer.
- *  @return A pointer to the surface allocated.
+ *	@param text The string to be printed on the surface.
+ *	@param f Font to render the text with.
+ *	@param c The color to render the text in.
+ *	@return A pointer to the surface allocated with "text" printed with given font "f"and color "c".
  */
 SDL_Surface	*get_text_surface(const char *text, t_font f, SDL_Color *c);
 
 /**
  *	Draws 'text' at a given 'orig' with font 'f' and color 'c'.
- *  @param text a constant character pointer.
- *  @param orig a SDL_Point structure pointer.
- *
- *  @param f TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
- *
- *  @param c a SDL_Color structure pointer.
+ *	@param text The string to be drawn on the surface.
+ *	@param orig The structures that defines the x and y location of the text's upper left corner.
+ *	@param f Font to render the text with.
+ *	@param c The color to render the text in.
  */
 void		draw_text(const char *text, SDL_Point *orig, t_font f, SDL_Color *c);
 
