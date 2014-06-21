65d10a6e136f2cf552b9395ebdc1687990c80299
Ennebati <y.ennebati@gmail.com>
2014-06-21 02:00:18 +0200

    [Doc/folderGame] Added annotations for graphic_handler.h

--- src/game/graphic_handler.h
+++ src/game/graphic_handler.h
@@ -5,7 +5,7 @@
 ** Login   <ahamad_s@etna-alternance.net>
 ** 
 ** Started on  Thu May  1 18:21:08 2014 AHAMADA Samir
-** Last update Thu Jun 19 22:22:55 2014 AHAMADA Samir
+** Last update Sat Jun 21 01:59:49 2014 ENNEBATI Yassine
 */
 
 /**
@@ -20,10 +20,27 @@
 #include <SDL2/SDL_render.h>
 #include "graphics.h"
 
+/**
+ *	Create a texture for a rendering context
+ *	@param r The rendering context
+ *	@param a An enumeration of texture access patterns
+ *	@param w The width of the texture in pixels
+ *	@param h The height of the texture in pixels
+ *	@return A pointer to the created texture
+ */
 t_texture	*make_texture(SDL_Renderer *r, Sint32 a, Sint32 w, Sint32 h);
 
+/**
+ *	Create a texture from an existing surface
+ *	@param r The rendering context
+ *	@param s The structure containing pixel data used to fill the texture
+ *	@return A pointer to the created texture
+ */
 t_texture	*make_texture_from_surface(SDL_Renderer *r, SDL_Surface *s);
 
+/**
+ *	Free all allocated ressources
+ */
 void		free_texture(t_texture *t);
 
 #endif /* !GRAPHIC_HANDLER_H */
