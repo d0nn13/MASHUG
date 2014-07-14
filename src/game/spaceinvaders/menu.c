b8e81a2df56adb433e4149fa47931cd54a170392
YasssE <y.ennebati@gmail.com>
2014-07-14 02:19:21 +0200

    [dev/yass/review] Updated indentation for game/spaceinvaders/*.c files

--- src/game/spaceinvaders/menu.c
+++ src/game/spaceinvaders/menu.c
@@ -5,7 +5,7 @@
 ** Login   <ahamad_s@etna-alternance.net>
 **
 ** Started on  Tue Apr 29 14:26:54 2014 AHAMADA Samir
-** Last update Thu Jul 10 19:43:19 2014 ENNEBATI Yassine
+** Last update Mon Jul 14 02:18:05 2014 ENNEBATI Yassine
 */
 
 #include <SDL2/SDL.h>
@@ -25,20 +25,20 @@
  *	Menu item definition
  */
 typedef enum
-  {
-    START_MEN,
-    SCORE_MEN,
-    NB_MEN
-  }		t_menuitem;
+{
+  START_MEN,
+  SCORE_MEN,
+  NB_MEN
+}		t_menuitem;
 
 /**
  *	Menu callback initialization
  */
 static t_mode		select[NB_MEN] =
-  {
-    &game_loop,
-    &hiscores
-  };
+{
+  &game_loop,
+  &hiscores
+};
 
 static t_menuitem	item = START_MEN;
 
@@ -62,18 +62,18 @@ void			menu_game()
   redraw_context(NULL);
   display_menu();
   while (g_launcher == &menu_game)
+  {
+    if (SDL_WaitEvent(&e))
     {
-      if (SDL_WaitEvent(&e))
-	{
-	  if (e.type == SDL_QUIT)
-	    g_launcher = NULL;
-	  if (e.type == SDL_KEYDOWN)
-	    {
-	      s = e.key.keysym.scancode;
-	      process_input(&s, &item);
-	    }
-	}
+      if (e.type == SDL_QUIT)
+	g_launcher = NULL;
+      if (e.type == SDL_KEYDOWN)
+      {
+	s = e.key.keysym.scancode;
+	process_input(&s, &item);
+      }
     }
+  }
 }
 
 static void	process_input(const SDL_Scancode *s, t_menuitem *item)
@@ -84,21 +84,21 @@ static void	process_input(const SDL_Scancode *s, t_menuitem *item)
   *item += (*s == UP && *item != START_MEN) ? -1 : 0;
   *item += (*s == DN && *item != SCORE_MEN) ? 1 : 0;
   if (*s == SDL_SCANCODE_ESCAPE)
-    {
-      g_launcher = &main_menu;
-      play_sfx(get_common_sfx(BLIPCANCEL_SFX));
-    }
+  {
+    g_launcher = &main_menu;
+    play_sfx(get_common_sfx(BLIPCANCEL_SFX));
+  }
   else if (*s == SDL_SCANCODE_RETURN || *s == SDL_SCANCODE_KP_ENTER)
-    {
-      g_launcher = select[*item];
-      play_sfx(get_common_sfx(BLIPOK_SFX));
-      redraw_context(NULL);
-    }
+  {
+    g_launcher = select[*item];
+    play_sfx(get_common_sfx(BLIPOK_SFX));
+    redraw_context(NULL);
+  }
   if (*item != old_item)
-    {
-      display_menu();
-      play_sfx(get_common_sfx(BLIPSEL_SFX));
-    }
+  {
+    display_menu();
+    play_sfx(get_common_sfx(BLIPSEL_SFX));
+  }
 }
 
 static void	display_menu()
