beca0d753a2b3b2358295baff9d8e22e8598aa9f
Samir Ahamada <ahamad_s@etna-alternance.net>
2014-07-25 14:15:37 +0200

    [dev/xml] Minor refactor + added prototypes and body of ibra's functions

--- src/core/handlers/xml_handler.c
+++ src/core/handlers/xml_handler.c
@@ -18,18 +18,40 @@
 
 #include "../handlers.h"
 
-static void	silent_xml(void *, char const *, ...);
 static t_xml_typeholder	const	types[] = {
   {&xml_hiscore_callback, "<!DOCTYPE hiscores SYSTEM \"dtd/hiscores.dtd\">"},
   {&xml_spritesheet_callback, "<!DOCTYPE img SYSTEM \"../spritesheet.dtd\">"}
 };
+
+/**
+ *	@brief	Checks if the XML file contains an DTD reference 
+ *
+ *	@return	Zero if the file contains a DTD reference, 1 otherwise
+ */
+static Uint8	xml_check_dtd(char const *path, t_xml_type t);
+
+/**
+ *	@brief	Inject the proper DTD reference in an XML file
+ */
+static void	xml_inject_dtd(char const *path, t_xml_type t);
+
+/*
+ *	@brief	Validates an XML file with DTD informations
+ *	@return	1 if the XML file is valid, 0 otherwise
+ */
+static Uint8	xml_validate(xmlDocPtr doc);
+
+/*
+ *	@brief	libxml2 error silencing callback
+ */
+static void	xml_silent(void *, char const *, ...);
 
 Uint8		xml_parse(char const *path, t_xml_type t, void *container)
 {
   xmlDocPtr	doc;
   xmlNodePtr	node;
 
-  xmlSetGenericErrorFunc(NULL, &silent_xml);
+  xmlSetGenericErrorFunc(NULL, &xml_silent);
   if (!ptr_chk(path, "xml path", XML_LCAT, "xml_parse") ||
       !ptr_chk(&types[t].call, "callback", XML_LCAT, "xml_parse"))
     return (0);
@@ -46,7 +68,40 @@ Uint8		xml_parse(char const *path, t_xml_type t, void *container)
   return (types[t].call(node, container));
 }
 
-static void	silent_xml(void *ctx, char const *msg, ...)
+static Uint8	xml_check_dtd(char const *path, t_xml_type t)
+{
+  (void)path;
+  (void)t;
+  return (1);
+}
+
+static void	xml_inject_dtd(char const *path, t_xml_type t)
+{
+  (void)path;
+  (void)t;
+}
+
+static Uint8		xml_validate(xmlDocPtr doc)
+{
+  xmlValidCtxtPtr	ctxt;
+  Uint8			r;
+
+  ctxt = xmlNewValidCtxt();
+  if (!ptr_chk(ctxt, "ctxt", XML_LCAT, "xml_validate"))
+  {
+    SDL_LogError(XML_LCAT, "Couldn't validate XML file");
+    return (0);
+  }
+  if ((r = xmlValidateDocument(ctxt, doc)))
+    SDL_LogVerbose(XML_LCAT, "xml_validate: '%s' has been validated",
+		   (char *)doc->name);
+  else
+    SDL_LogError(XML_LCAT, "Couldn't validate XML file");
+  free(ctxt);
+  return (r);
+}
+
+static void	xml_silent(void *ctx, char const *msg, ...)
 {
   (void)ctx;
   (void)msg;
