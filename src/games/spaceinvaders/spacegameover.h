d4d50b11f66068ec18f9e70548bdf19b2c14071b
YassineE <y.ennebati@gmail.com>
2014-09-06 13:27:13 +0200

    [dev/spacegameover] Spaceinvaders game over implementation started

---
+++ src/games/spaceinvaders/spacegameover.h
@@ -0,0 +1,21 @@
+/*
+** spacegameover.h for MASHUG in /Users/Yassine/Code/ETNA/projet/c/mashug/src/games/spaceinvaders
+** 
+** Made by ENNEBATI Yassine
+** Login   <enneba_y@etna-alternance.net>
+** 
+** Started on  Sat Sep  6 12:31:59 2014 ENNEBATI Yassine
+** Last update Sat Sep  6 12:53:48 2014 ENNEBATI Yassine
+*/
+
+/**
+ *	@file	spacegameover.h
+ *	@brief	Game over for space invaders game
+ */
+
+#ifndef GAMES_SPACEINVADERS_SPACEGAMEOVER_H_
+# define GAMES_SPACEINVADERS_SPACEGAMEOVER_H_
+
+void	space_gameover();
+
+#endif /* !GAMES_SPACEINVADERS_SPACEGAMEOVER_H_ */
