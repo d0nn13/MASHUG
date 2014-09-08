8d654cad053f4beb1cec8d09dc4e20e39a1b1b1b
Ibrahim FOFANA <fofana_i@etna-alternance.net>
2014-09-08 19:04:13 +0200

    [dev/ibra/ufo] UFO appears on a randomly side during the game maneged by an elapsed time

--- src/games/spaceinvaders/objects/ufo_callback.c
+++ src/games/spaceinvaders/objects/ufo_callback.c
@@ -5,7 +5,7 @@
 ** Login   <fofana_i@etna-alternance.net>
 **
 ** Started on  Sun Sep  7 15:14:45 2014 FOFANA Ibrahim
-** Last update Mon Sep  8 17:58:31 2014 FOFANA Ibrahim
+** Last update Mon Sep  8 19:08:49 2014 FOFANA Ibrahim
 */
 
 #include "../../../core/handlers.h"
@@ -13,28 +13,57 @@
 
 #include "ufo_callback.h"
 
-#define	UFO_VEL	1
-#define UFO_MAX_X (get_spacebounds()->x + get_spacebounds()->w)
-#define UFO_MIN_X (get_spacebounds()->x)
+#include <stdlib.h>
 
-void	spaceufo_display(t_spaceufo const *ufo)
+#define	UFO_VEL 1
+#define	UFO_MAX_X (get_spacebounds()->x + get_spacebounds()->w + 100)
+#define	UFO_MIN_X (get_spacebounds()->x - 100)
+#define	UFO_ELAPSED_TIME (40000 + rand() % 40000)
+
+static	void	spaceufo_appear(t_spaceufo *ufo);
+
+static	Uint32	ti = 0;
+
+void		spaceufo_display(t_spaceufo const *ufo)
 {
   if (ufo->direction != UFO_NONE)
     draw_sprite(ufo->sprite, &ufo->rect);
 }
 
-void	spaceufo_move(t_spaceufo *ufo)
+void		spaceufo_move(t_spaceufo *ufo)
 {
   if (ufo->direction == UFO_NONE)
-    return ;
+    spaceufo_appear(ufo);
 
-  if (ufo->rect.x > UFO_MAX_X + (ufo->rect.w / 2))
-    ufo->direction = UFO_LEFT;
-  if (ufo->rect.x <  UFO_MIN_X - (ufo->rect.w * 1.5))
-    ufo->direction = UFO_RIGHT;
+  if (ufo->rect.x > UFO_MAX_X)
+    ufo->direction = UFO_NONE;
+  if (ufo->rect.x <  UFO_MIN_X)
+    ufo->direction = UFO_NONE;
 
   if (ufo->direction == UFO_RIGHT)
     ufo->rect.x += UFO_VEL;
   else if (ufo->direction == UFO_LEFT)
     ufo->rect.x -= UFO_VEL;
+}
+
+static	void	spaceufo_appear(t_spaceufo *ufo)
+{
+  Uint32	to;
+
+  to = SDL_GetTicks();
+
+  if ((to - ti) > UFO_ELAPSED_TIME)
+  {
+    ti = SDL_GetTicks();
+    if (rand() % 2)
+    {
+      ufo->direction = UFO_RIGHT;
+      ufo->rect.x = UFO_MIN_X;
+    }
+    else
+    {
+      ufo->direction = UFO_LEFT;
+      ufo->rect.x = UFO_MAX_X;
+    }
+  }
 }
